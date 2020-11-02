1871f84ce8ff48f5eaf762e68ab67db794c89b5f
ChristosBaztekas <72806541+ChristosBaztekas@users.noreply.github.com>
2020-11-02 15:30:57 +0200

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,5 +1,34 @@
 package gr.projAboutCovid.leo.proj;
-
+import java.util.Scanner;
 public class Human {
+	//οι ανθρωποι ειναι σε αλφαβητικοι σειρα
+	private /* list of human*/ static Human allHuman;
+	private String id;
+	public Human() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Give id: ");
+		String id = sc.nextLine();
+		if(! isInBase(id)) {
+			apothikeysi(
+	}
+	public static Human/*list*/[] inputHuman() {
+		//input peopple of an organazation
+		human = /*list of human*/
+		for(;;) {
+			System.out.println("Another input?yes or no")
+			Scanner sc = new Scanner(System.in);
+			ans = sc.nextLine();
+			if (ans == 'Yes' || ans =='yes') {
+				human[1] = new Human();
+			}
+		}
+	}
+	private static boolean isInBase(String id){
+		//diadiki anazitisi stin static list allHuman
+		while(){
+		}
+		return false;
 
+	}
+	private static void apothikeysi
 }
