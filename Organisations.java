d83259c6352d96721c8a9d0854ae3016c33121e0
ChristosBaztekas <72806541+ChristosBaztekas@users.noreply.github.com>
2020-11-02 16:03:13 +0200

    Update Organisations.java

--- Organisations.java
+++ Organisations.java
@@ -1,5 +1,35 @@
 package gr.projAboutCovid.leo.proj;
 
-public class Organisations {
-
-}
+public abstact class Organisations {
+    private String name;
+    private String area;
+    private int id;
+    private static int count = 0;
+    public Organisations(String name, String area, int numbersOfPeople) {
+       this.name = name;
+       this.area = area;
+       count++;
+       this.id = count;
+    }
+   public int getId() {
+     return id;
+    }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+     this.name = name;
+  }
+  public String getArea() {
+    return area;
+  }
+ public void setArea(String name) {
+   this.name = area;
+ }
+ // can allhuman or one human 
+  // απο εδω και κατω βλεπουμε
+ public abstact void people(Human human);
+  
+ public abstarct /*vlepoyme*/ void  managementCases();
+ 
+ public abstract 
