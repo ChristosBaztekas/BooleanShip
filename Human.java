cc8d29d00f354758578a1a8ae3277a86ec9ebbd9
ChristosBaztekas <christosbaztekas@gmail.com>
2020-11-06 14:30:04 +0200

    change findIndex method

--- Human.java
+++ Human.java
@@ -8,10 +8,11 @@ public class Human {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Give id: ");
 		String id = sc.nextLine();
-		if(! isInBase(id)) {
+		if(findIndex(id) == -1) {
 			apothikeysi(
+		}
 	}
-	public static Human/*list*/[] inputHuman() {
+	public static ArrayList<Human> inputHuman() {
 		//input peopple of an organazation
 		human = /*list of human*/
 		for(;;) {
@@ -23,12 +24,28 @@ public class Human {
 			}
 		}
 	}
-	private static boolean isInBase(String id){
+	// returns -1 if already exists
+	private static int findIndex(String id){
 		//diadiki anazitisi stin static list allHuman
-		while(){
+			int left = 1;
+			int right = allHuman.size();
+			int middle;
+			boolean flag= false;
+		while(flag && left <= right) {
+			middle = (left + right)/2;
+			if (allHuman.get(middle).getId() == id) {
+				return middle;
+			} else if (allHuman.get(middle).getId() < id) {
+				left = middle + 1;
+			} else {
+				right = middle -1;
+			}
 		}
-		return false;
-
+		return -1;
 	}
+	// maybe change public access
+	public int getId() {
+			return id;
+		}
 	private static void apothikeysi
 }
