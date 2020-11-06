ec3983ddb81c0c679263670d0aaa0e02f4b83370
ChristosBaztekas <christosbaztekas@gmail.com>
2020-11-06 12:52:55 +0200

    constructor & 1st method
    first try in constructor and one method

--- NursingHomes.java
+++ NursingHomes.java
@@ -1,5 +1,14 @@
 package gr.projAboutCovid.leo.proj;
 
 public class NursingHomes extends Organisations {
-        // a comment
+    String status_descr;
+    boolean status; //true means enclosed and false free access
+    public NursingHomes(String name, String area, int numberOfPeople, boolean enclosed) {
+        super(name, area, numberOfPeople);
+        status = enclosed;
+        status_descr = (enclosed ? "Enclosed" : "Free Access");
+    }
+    private void monitoring() {
+        
+    }
 }
