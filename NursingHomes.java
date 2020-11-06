f19b8ab0fc8f4f11a0bc17d0852b6f5e5fbafd31
ChristosBaztekas <christosbaztekas@gmail.com>
2020-11-06 14:29:43 +0200

    filling employs method
    first map of fiilingEmploy method

--- NursingHomes.java
+++ NursingHomes.java
@@ -1,12 +1,32 @@
 package gr.projAboutCovid.leo.proj;
-
+import java.util.Scanner;
 public class NursingHomes extends Organisations {
     String status_descr;
+    ArrayList<Human> employs;
+    Arraylist<Human> carenPeople;
     boolean status; //true means enclosed and false free access
     public NursingHomes(String name, String area, int numberOfPeople, boolean enclosed) {
         super(name, area, numberOfPeople);
         status = enclosed;
         status_descr = (enclosed ? "Enclosed" : "Free Access");
+        fillingEmploys();
+        fillingCarenPeople();//yet to do
+    }
+    private void fillingEmploys() {
+        Scanner scanner = new Scanner(System.in);
+        String ans;
+        int num;
+        if(employs != null) {
+            System.out.printf("There are %d Employ(s), would you like to add new?/nPress yes or no", employs.size());
+            ans = scanner.nextLine();
+            if (ans == "yes") {
+                System.out,println("How many? ");
+                num = scanner.nextInt();
+                for (int i=0 ; i < num ; i++) {
+                    employs.add()
+                }
+            }
+        }
     }
     private void monitoring() {
         
