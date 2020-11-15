6adb0d20f04a6312c2edcbe4340fe41421b691d7
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-15 21:22:43 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package gr.projAboutCovid.leo.proj;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JFrame myWindow = new JFrame("Καλωσήρθατε στην εφαρμογή ανίχνευσης κρουσμάτων και εντοπισμού επαφών!");
+		myWindow.setSize(900, 900);
+		myWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		myWindow.setVisible(true);
+
+		// setLayout(new FlowLayout());
+
+	}
+
+}
