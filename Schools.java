104c12780753c449fcd5bda43ccf1ab34487f2e2
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-23 11:05:46 +0200

    Update Schools.java

--- Schools.java
+++ Schools.java
@@ -2,4 +2,27 @@ package gr.projAboutCovid.leo.proj;
 
 public class Schools extends Organisations{
 
+	public Schools(String name, String area, int numbersOfPeople) {
+		super(name, area, numbersOfPeople);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insertHumanToOrganisation(Human human) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void managementCases(Human anyH) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void declareCase() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
