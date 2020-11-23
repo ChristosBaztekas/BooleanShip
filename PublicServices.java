961074f78876d12b931fb3f6551e3607626e61e3
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-23 11:04:42 +0200

    Update PublicServices.java

--- PublicServices.java
+++ PublicServices.java
@@ -2,4 +2,9 @@ package gr.projAboutCovid.leo.proj;
 
 public class PublicServices extends Labors {
 
+	public PublicServices(String name, String area, int numbersOfPeople) {
+		super(name, area, numbersOfPeople);
+		// TODO Auto-generated constructor stub
+	}
+
 }
