0a7700e101ca6e103eb795713865afea02510d93
ChristosBaztekas <christosbaztekas@gmail.com>
2020-11-23 23:00:23 +0200

    δημιουργεια modifyEmploys()

--- NursingHomes.java
+++ NursingHomes.java
@@ -14,43 +14,85 @@ public class NursingHomes extends Organisations {//απλά έκανα implement
     private ArrayList<Human> carenPeople;//υπήρχε συνταχτικό λάθος
     static Scanner scanner = new Scanner(System.in);
     boolean status; //true means enclosed and false free access
-    private static int count = 0;
     private final int id;
+    private static ArrayList<NursingHomes> = new ArrayList<NursingHomes>();
+    private static int count = 0;
     public NursingHomes(String name, String area, int numberOfPeople, boolean enclosed) {
         super(name, area, numberOfPeople);
         status = enclosed;
         status_descr = (enclosed ? "Enclosed" : "Free Access");
         id = count ++;
-        fillingEmployees();
+        modifyEmployees();
         fillingCarenPeople();//yet to do
     }
-    private void fillingEmployees() {
-        String ans;
-        int num;
-        if(employees != null) {
-            System.out.printf("There are %d Employee(s), would you like to add new?/nPress yes or no", employees.size());
-            ans = scanner.nextLine();
-            if (ans.equals("yes")) {
-                System.out.println("How many? ");
-                num = scanner.nextInt();
-                for (int i=0 ; i < num ; i++) {
-                    System.out.printf(Adding the %d employ, i+1);
-                    Human one = createHuman(this.getClass().getName(), this.id);
-                    if(one == null)
-                    employees.add();
+    private void modifyEmployees() {
+        while (true){
+            if(employees != null) {
+                System.out.printf("There are %d Employee(s), would you like to add new?\nPress 1 for yes,otherwise we will go to remove panel", employees.size());
+                int ans = scanner.nextInt();
+                if (ans == 1) {
+                    System.out.println("How many? ");
+                    int num = scanner.nextInt();
+                    for (int i=0 ; i < num ; i++) {
+                        System.out.printf("Adding the %d employ", i+1);
+                        Human one = createHuman(this.getClass().getName(), this.id);
+                        if(one == null){
+                            System.out.printlf("Δεν προσθέθηκε ο %d υπάλληλος", i+1);
+                            i -=1;//να επαναληφθεί η διαδικασία
+                            continue;
+                        }
+                        employees.add(one);
+                    }
+                    break;
                 }
+                System.out.printf("There are %d Employee(s), would you like to remove one?\nPress 1 for yes", employees.size());
+                int ans = scanner.nextInt();
+                if (ans == 1) {
+                    while (true){
+                        System.out.println("Give the afm of the employee that want to remove");
+                        String afm = scanner.nextLine();
+                        boolean flag;
+                        for (int i=0; i<employees.size(); i++) {
+                            //search
+                            //an to vrei prepei na paei stin human na svisei to belongsOrganisation
+                            flag = false;
+                            break;
+                        }
+                        if (flag) {
+                            System.out.printf("Δεν βρεθηκε υπαλληλος με αφμ:%s", afm);
+                        }
+                        System.out.println("Να συνεχιστεί η διαδικασία διαγγραφής; 0 για εξοδο");
+                        int ans1 = scanner.nextInt();
+                        if (ans1 == 0) {
+                            break;
+                        }
+                    }
+                    break;
+                }
+            } else {
+                System.out.println("There are no employes filled, Please start with giving them");
+                System.out.println("Ποσοι δουλεύουνε;");
+                int num = scanner.nextInt();
+                for (int i=0 ; i < num ; i++) {
+                    System.out.printf("Creating the %d employ", i+1);
+                    Human one = createHuman(this.getClass().getName(), this.id);
+                    if(one == null){
+                        System.out.printlf("Δεν προσθέθηκε ο %d υπάλληλος", i+1);
+                        i -=1;//να επαναληφθεί η διαδικασία
+                        continue;
+                    }
+                    employees.add(one);
+                }
+                break;
             }
-        } else {
-            System.out.println("There are no employes filled, Please start with giving them");
-
         }
     }
-    private void monitoring() {
+    public void monitoring(int orgNum) {
         
     }
     private void declareCase(){
         System.out.println("Is the case Employee(s), caren person(s) or both");
-        System.out.println("Enter       E,          C,                B");//έλλειπε απλά το ερωτηματικό
+        System.out.println("Enter       E,          C,                B");
         String input = scanner.nextLine();
         if(input.equals('E')){
 
@@ -61,6 +103,9 @@ public class NursingHomes extends Organisations {//απλά έκανα implement
         } else {
 
         }
+    }
+    private void fillingCarenPeople() {
+        
     }
     @Override
     private void insertHumanToOrganisation(Human human) {
