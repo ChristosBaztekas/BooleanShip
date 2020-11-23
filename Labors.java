92df98937e1099244f1cc0e23e311193f09954b6
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-23 11:03:42 +0200

    Update Labors.java

--- Labors.java
+++ Labors.java
@@ -2,4 +2,27 @@ package gr.projAboutCovid.leo.proj;
 
 public class Labors extends Organisations {
 
+	public Labors(String name, String area, int numbersOfPeople) {
+		super(name, area, numbersOfPeople);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insertHumanToOrganisation(Human human) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void managementCases(Human anyH) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void declareCase() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
