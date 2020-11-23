61000c3248803553b55de42bd4ec17df21927c92
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-23 10:34:11 +0200

    Update NursingHomes.java
    Διόρθωση στον χειρισμό των String(το == είναι λάθος , το equals χρησιμοποιείται για Strings) και κάποιων απλών συνταχτικών λαθών.

--- NursingHomes.java
+++ NursingHomes.java
@@ -1,15 +1,18 @@
 package gr.projAboutCovid.leo.proj;
-make
+
 import java.util.Scanner;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
 import java.util.ArrayList;
 //αν μπορούμε να γράφουμε σχόλια για είναι ο κώδικας ποιό ευανάγνωστος
 //έχω διορθώσει κάποια συντακτικά λάθη κυρίως 
 //προτίνω να ελένχουμε τον κώδικα με κάποιο IDE ή τουλάχιστον TextPad
-public class NursingHomes extends Organisations {
+public class NursingHomes extends Organisations {//απλά έκανα implement τις abstract γιατί αλλιώς δίνει error
     private String status_descr;
     private ArrayList<Human> employees;
-    private Arraylist<Human> carenPeople;
-    static Scanner scanner = new Scanner(System.out);
+    private ArrayList<Human> carenPeople;//υπήρχε συνταχτικό λάθος
+   static Scanner scanner = new Scanner(System.in);//System.out??
     boolean status; //true means enclosed and false free access
     public NursingHomes(String name, String area, int numberOfPeople, boolean enclosed) {
         super(name, area, numberOfPeople);
@@ -24,7 +27,7 @@ public class NursingHomes extends Organisations {
         if(employees != null) {
             System.out.printf("There are %d Employee(s), would you like to add new?/nPress yes or no", employees.size());
             ans = scanner.nextLine();
-            if (ans == "yes") {
+            if (ans.equals("yes")) {//στην java για να χειρίζεσαι Strings δεν λειτουργεί το == αλλά το equals 
                 System.out.println("How many? ");
                 num = scanner.nextInt();
                 for (int i=0 ; i < num ; i++) {
@@ -38,13 +41,13 @@ public class NursingHomes extends Organisations {
     }
     private void declareCase(){
         System.out.println("Is the case Employee(s), caren person(s) or both");
-        System.out.println("Enter       E,          C,                B")
+        System.out.println("Enter       E,          C,                B");//έλλειπε απλά το ερωτηματικό
         String input = scanner.nextLine();
-        if(input = 'E'){
+        if(input.equals('E')){
 
-        } else if (input = 'C'){
+        } else if (input.equals('C')){
 
-        } else if (input = "B"){
+        } else if (input.equals('B')){
 
         } else {
 
@@ -54,4 +57,19 @@ public class NursingHomes extends Organisations {
     private void insertHumanToOrganisation(Human human) {
         //
     }
+	@Override
+	public void insertHumanToOrganisation(Human human) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void managementCases(Human anyH) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void declareCase() {
+		// TODO Auto-generated method stub
+		
+	}
 }
