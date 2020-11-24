37b8a7f8cef3426386bf39b24494690a6ecfcf67
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-24 15:05:23 +0200

    Update Companies.java

--- Companies.java
+++ Companies.java
@@ -1,11 +1,27 @@
 package gr.projAboutCovid.leo.proj;
 
-public class Companies extends Labors {
+public class Companies extends Labors implements caseManagmentAndHumanAddition {
 
 	public Companies(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
 		// TODO Auto-generated constructor stub
 	}
-	//Απαραίτητο είναι να υλοποιηθούν και εδώ οι abstract παρόλο που δεν σε αναγκάζει η java
+	@Override
+	public void insertHumanToOrganisation(Human human) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void managementCases(Human anyH) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void declareCase() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
