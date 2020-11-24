62cbd5b6a6866c403976db0eaa3d742981090fe6
Nikoluckies <72814467+NikolaosKiouftis@users.noreply.github.com>
2020-11-25 01:32:46 +0200

    Add files via upload
    Δημιουργία HigherEducation

---
+++ HigherEducation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package projectCovid19.nikoluckies;
+
+/**
+ * @author Nikoluckies
+ *
+ */
+public class HigherEducation extends Schools {
+
+	/**
+	 * 
+	 */
+	public HigherEducation(String name, String area, int numbersOfPeople) {
+			super(name, area, numbersOfPeople);
+		// TODO Auto-generated constructor stub
+	}
+
+}
