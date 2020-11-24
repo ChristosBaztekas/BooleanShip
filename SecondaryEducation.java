2e0bce7f3933280f2c854056e5fb1f883da38740
Nikoluckies <72814467+NikolaosKiouftis@users.noreply.github.com>
2020-11-25 01:15:52 +0200

    Add files via upload
    Δημιουργία SecondaryEducation

---
+++ SecondaryEducation.java
@@ -0,0 +1,10 @@
+package projectCovid19.nikoluckies;
+
+public class SecondaryEducation extends Schools {
+
+	public SecondaryEducation(String name, String area, int numbersOfPeople) {
+			super(name, area, numbersOfPeople);
+		// TODO Auto-generated constructor stub
+	}
+
+}
