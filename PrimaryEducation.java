b12fcd0c088670e905eddc0e37fb637a676881a1
Nikoluckies <72814467+NikolaosKiouftis@users.noreply.github.com>
2020-11-25 01:04:08 +0200

    Add files via upload
    Δημιουργία PrimaryEducation

---
+++ PrimaryEducation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package projectCovid19.nikoluckies;
+
+/**
+ * @author Nikoluckies
+ *
+ */
+public class PrimaryEducation extends Schools {
+
+	/**
+	 * 
+	 */
+	public PrimaryEducation(String name, String area, int numbersOfPeople) {
+			super(name, area,numbersOfPeople);
+		// TODO Auto-generated constructor stub
+	}
+
+}
