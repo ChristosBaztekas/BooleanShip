7b158ab6570a8fa4e8c9442a53ff835f186d15da
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-24 15:12:56 +0200

    Update PublicServices.java

--- PublicServices.java
+++ PublicServices.java
@@ -1,10 +1,26 @@
 package gr.projAboutCovid.leo.proj;
 
-public class PublicServices extends Labors {
+public class PublicServices extends Labors implements caseManagmentAndHumanAddition{
 
 	public PublicServices(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
 		// TODO Auto-generated constructor stub
 	}
+	@Override
+	public void insertHumanToOrganisation(Human human) {
+		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void managementCases(Human anyH) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void declareCase() {
+		// TODO Auto-generated method stub
+		
+	}
 }
