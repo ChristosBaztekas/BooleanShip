47b09196adfe265a1fecc31e061a5a9ea44aa98a
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-25 13:51:05 +0200

    Update Universities.java

--- Universities.java
+++ Universities.java
@@ -4,24 +4,24 @@ public class Universities extends Organisations implements caseManagmentAndHuman
 
 	public Universities(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	
 	public void insertHumanToOrganisation(Human human) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	
 	public void managementCases(Human anyH) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	
 	public void declareCase() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
