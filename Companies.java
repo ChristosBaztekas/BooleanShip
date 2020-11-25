8872c51fe2dada173d0610a9f2f084e3dd97b75a
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-25 13:37:46 +0200

    Update Companies.java

--- Companies.java
+++ Companies.java
@@ -4,23 +4,23 @@ public class Companies extends Labors implements caseManagmentAndHumanAddition {
 
 	public Companies(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
-		// TODO Auto-generated constructor stub
+		
 	}
 	@Override
 	public void insertHumanToOrganisation(Human human) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void managementCases(Human anyH) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void declareCase() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
