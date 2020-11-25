5de95c9334c6dac65637638ee21d49d1d8c7cdd7
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-25 13:47:01 +0200

    Update PublicServices.java

--- PublicServices.java
+++ PublicServices.java
@@ -4,23 +4,23 @@ public class PublicServices extends Labors implements caseManagmentAndHumanAddit
 
 	public PublicServices(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
-		// TODO Auto-generated constructor stub
+		
 	}
 	@Override
 	public void insertHumanToOrganisation(Human human) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void managementCases(Human anyH) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
 	@Override
 	public void declareCase() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 }
