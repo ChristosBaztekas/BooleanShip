e8942b7ba539a630ec9d32cb2369b1fcd9107e92
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-25 13:35:23 +0200

    Update SecondaryEducation.java

--- SecondaryEducation.java
+++ SecondaryEducation.java
@@ -1,10 +1,31 @@
 package projectCovid19.nikoluckies;
 
-public class SecondaryEducation extends Schools {
+public class SecondaryEducation extends Schools implements caseManagmentAndHumanAddition {
 
+	
 	public SecondaryEducation(String name, String area, int numbersOfPeople) {
-			super(name, area, numbersOfPeople);
+		super(name, area, numbersOfPeople);
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+	public void insertHumanToOrganisation(Human human) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void managementCases(Human anyH) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void declareCase() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	
 }
