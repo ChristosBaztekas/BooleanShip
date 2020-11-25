88432b3f47759cab927c16dd3224cee2d3d22121
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-25 13:34:25 +0200

    Update PrimaryEducation.java

--- PrimaryEducation.java
+++ PrimaryEducation.java
@@ -7,14 +7,31 @@ package projectCovid19.nikoluckies;
  * @author Nikoluckies
  *
  */
-public class PrimaryEducation extends Schools {
+public class PrimaryEducation extends Schools implements caseManagmentAndHumanAddition {
 
-	/**
-	 * 
-	 */
 	public PrimaryEducation(String name, String area, int numbersOfPeople) {
-			super(name, area,numbersOfPeople);
-		// TODO Auto-generated constructor stub
+		super(name, area, numbersOfPeople);
+		
+	}
+	
+	@Override
+	public void insertHumanToOrganisation(Human human) {
+		
+		
 	}
 
+	@Override
+	public void managementCases(Human anyH) {
+		
+		
+	}
+
+	@Override
+	public void declareCase() {
+		
+		
+	}
+	
+
+	
 }
