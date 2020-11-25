8d87df0141ab849d6ef3eb9fc9c6dca2a20ad3d2
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-25 12:32:31 +0200

    Update Universities.java

--- Universities.java
+++ Universities.java
@@ -7,19 +7,19 @@ public class Universities extends Organisations {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
+	
 	public void insertHumanToOrganisation(Human human) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
+	
 	public void managementCases(Human anyH) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
+	
 	public void declareCase() {
 		// TODO Auto-generated method stub
 		
