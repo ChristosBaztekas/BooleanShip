f9c934ff66af480b0cf37eb2e07fea12fa0554b5
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-11-25 13:47:41 +0200

    Update Schools.java

--- Schools.java
+++ Schools.java
@@ -4,24 +4,24 @@ public class Schools extends Organisations implements caseManagmentAndHumanAddit
 
 	public Schools(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
-		// TODO Auto-generated constructor stub
+		
 	}
 
-	@Override
+	
 	public void insertHumanToOrganisation(Human human) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
-	@Override
+	
 	public void managementCases(Human anyH) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
-	@Override
+	
 	public void declareCase() {
-		// TODO Auto-generated method stub
+		
 		
 	}
 
