f5f3c77bada22f16160fd035e05e51c8f1911019
Nikoluckies <72814467+NikolaosKiouftis@users.noreply.github.com>
2020-11-28 19:38:18 +0200

    Add files via upload

---
+++ Businesses.java
@@ -0,0 +1,12 @@
+package projectCovid19.nikoluckies;
+
+import java.util.Scanner;
+
+
+public class Businesses extends Labors {
+
+	public Businesses() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
