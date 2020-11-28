c31eb9b56b74c8bcd05992547064d0482785ba65
Nikoluckies <72814467+NikolaosKiouftis@users.noreply.github.com>
2020-11-28 19:36:08 +0200

    Update PublicServices.java

--- PublicServices.java
+++ PublicServices.java
@@ -1,11 +1,14 @@
 package gr.projAboutCovid.leo.proj;
 
+import java.util.Scanner;
+
+
 public class PublicServices extends Labors {
 	private final int id;
 	private static int count = 0;
 	private ArrayList<Human> employees;
 	static Scanner scanner = new Scanner(System.in);
-	private static ArrayList<PublicServices> = new ArrayList<PublicServices>();
+	private static ArrayList<PublicServices> a = new ArrayList<PublicServices>();
 
 	public PublicServices(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
