3a83a02212610f1d9fc23233e330407881fe4750
Nikoluckies <72814467+NikolaosKiouftis@users.noreply.github.com>
2020-11-28 19:55:46 +0200

    Update Businesses.java

--- Businesses.java
+++ Businesses.java
@@ -4,9 +4,16 @@ import java.util.Scanner;
 
 
 public class Businesses extends Labors {
+	private final int id;
+	private static int count = 0;
+	private ArrayList<Human> employees;
+	static Scanner scanner = new Scanner(System.in);
+	private static ArrayList<Businesses> a = new ArrayList<Businesses>();
 
-	public Businesses() {
-		// TODO Auto-generated constructor stub
+	public Businesses(String name, String area, int numbersOfPeople) {
+		super(name, area, numbersOfPeople);
+		count ++;
+		
 	}
 
 }
