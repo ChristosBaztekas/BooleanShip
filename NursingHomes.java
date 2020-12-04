73ce909d1445c2b80af395872518ff937f85e264
ChristosBaztekas <christosbaztekas@gmail.com>
2020-12-04 17:17:28 +0200

    formation of the NursingHomes

--- NursingHomes.java
+++ NursingHomes.java
@@ -1,26 +1,21 @@
 package gr.projAboutCovid.leo.proj;
 
 import java.util.Scanner;
-
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
-
 import java.util.ArrayList;
 
-//αν μπορούμε να γράφουμε σχόλια για είναι ο κώδικας ποιό ευανάγνωστος
-//έχω διορθώσει κάποια συντακτικά λάθη κυρίως 
-//προτίνω να ελένχουμε τον κώδικα με κάποιο IDE ή τουλάχιστον TextPad
 public class NursingHomes extends Organisations implements caseManagmentAndHumanAddition{
     private String status_descr;
-    private ArrayList<Human> employees;
-    private ArrayList<Human> carenPeople;//υπήρχε συνταχτικό λάθος
+    private ArrayList<Human> employees = new ArrayList<Human>();
+    private ArrayList<Human> carenPeople = new ArrayList<Human>();
     static Scanner scanner = new Scanner(System.in);
-    boolean status; //true means enclosed and false free access
+    private boolean en_status; //true means enclosed and false free access
     private final int id;
     private static ArrayList<NursingHomes> = new ArrayList<NursingHomes>();
     private static int count = 0;
     public NursingHomes(String name, String area, int numberOfPeople, boolean enclosed) {
         super(name, area, numberOfPeople);
-        status = enclosed;
+        en_status = enclosed;
         status_descr = (enclosed ? "Enclosed" : "Free Access");
         id = count ++;
         modifyEmployees();
@@ -108,17 +103,17 @@ public class NursingHomes extends Organisations implements caseManagmentAndHuman
     private void fillingCarenPeople() {
         
     }
-   
-	
-	@Override
-	public void managementCases(Human anyH) {
 
-		
-	}
-	@Override
-	public void insertHumanToOrganisation(Human human) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void managementCases() {
+        //look caseManagmentAndHumanAddition
+    }
+    public static void createOrg() {
+        //look caseManagmentAndHumanAddition
+    }
+    public static void declareCase(int number) {
+        //look caseManagmentAndHumanAddition
+    }
+    public static void printDetails(int number) {
+        //look caseManagmentAndHumanAddition
 	
 }
