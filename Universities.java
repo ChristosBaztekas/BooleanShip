f1a6ad46f15568f924ec716d81ccb13457446b65
ChristosBaztekas <christosbaztekas@gmail.com>
2020-12-04 17:16:30 +0200

    formation of the Uni

--- Universities.java
+++ Universities.java
@@ -6,39 +6,23 @@ public class Universities extends Organisations implements caseManagmentAndHuman
 		super(name, area, numbersOfPeople);
 		
 	}
-
-	
-	public void insertHumanToOrganisation(Human human) {
-		
-		
-	}
-
-	
-	public void managementCases(Human anyH) {
-		
-		
-	}
-
-	
-	public void declareCase() {
-		
-		
-	}
-	public static void createUni() {
-		//creates a new university
-		//Universities u = new Universities()
-	}
-	public static int check() {
-		//ask for the code , ask if wants to quit too
-		//checks if the code exists and returns it, otherwise returns -1
-		return 0;
-	}
 	public static void printDetails(int code) {
 		//this method is used in displayUMenu
 		//prints all details of the specific Uni(FE name,region)
 		//with the code that takes,the code is similar the the check method ahead
+		//look caseManagmentAndHumanAddition
 	}
 	public static void callDeclareCase(int code) {
 		//for example allUni.get(code).declareCase();
 	}
+	public void managementCases() {
+		//look caseManagmentAndHumanAddition
+	}
+	public static void createOrg() {
+		//look caseManagmentAndHumanAddition
+	}
+	public static void declareCase(int number) {
+		//look caseManagmentAndHumanAddition
+	}
+
 }
