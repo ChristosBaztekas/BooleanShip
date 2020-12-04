99d8f0112e06cd92d3a9215ca31697cc2d9333e0
ChristosBaztekas <christosbaztekas@gmail.com>
2020-12-04 17:16:50 +0200

    formation of the Schools

--- Schools.java
+++ Schools.java
@@ -9,40 +9,22 @@ public class Schools extends Organisations implements caseManagmentAndHumanAddit
 		
 	}
 
-	
-	public void insertHumanToOrganisation(Human human) {
-		
-		
-	}
-
-	
-	public void managementCases(Human anyH) {
-		
-		
-	}
-
-	
-	public void declareCase() {
-		
-		
-	}
-	public static void createSchool() {
-		//see if arguments will be taken from constructor
-		Schools school = new Schools("", "", ""); //not good statement
-												//trying to give an idea
-	}
-	public static int check() {
-		//ask for the code , ask if wants to quit too
-		//checks if the code exists and returns it, otherwise returns -1
-		return 0;
-	}
 	public static void printDetails(int code) {
 		//this method is used in displaySMenu
 		//prints all details of the specific school(FE name,region)
 		//with the code that takes,the code is similar the the check method ahead
+		//look caseManagmentAndHumanAddition
 	}
 	public static void callDeclareCase(int code) {
 		//for example allSchools.get(code).declareCase();
 	}
-
+	public void managementCases() {
+		//look caseManagmentAndHumanAddition
+	}
+	public static void createOrg() {
+		//look caseManagmentAndHumanAddition
+	}
+	public static void declareCase(int number) {
+		//look caseManagmentAndHumanAddition
+	}
 }
