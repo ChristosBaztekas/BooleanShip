a5223fa1257113d69cef98a754135b243970cd93
KonstantinosTsifis <72867475+tsfkostas@users.noreply.github.com>
2020-12-06 11:39:52 +0200

    Update Companies.java

--- Companies.java
+++ Companies.java
@@ -1,11 +1,14 @@
 package gr.projAboutCovid.leo.proj;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Companies extends Labors implements caseManagmentAndHumanAddition {
 	private final int id;
 	private static int count = 0;
 	private ArrayList<Human> employees;
 	static Scanner scanner = new Scanner(System.in);
-	private static ArrayList<Companies> a = new ArrayList<Companies>();
+	private static ArrayList<Companies> allCompanies = new ArrayList<Companies>();
 
 	public Companies(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
