94b57598cc506ee2908b388a7f38482775fec738
KonstantinosTsifis <72867475+tsfkostas@users.noreply.github.com>
2020-12-06 11:36:00 +0200

    New Class that is needed

---
+++ Classes.java
@@ -0,0 +1,11 @@
+package gr.projAboutCovid.leo.proj;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Classes {
+    private static Scanner scanner = new Scanner(System.in);
+    private String idifier;
+    private ArrayList<Human> students = new ArrayList<Human>();
+    public Classes(String class, int id){
+
+    }
+}
