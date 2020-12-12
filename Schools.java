3255bf3c5c09f3c1cf098a343aea320f6bb47a67
KonstantinosTsifis <72867475+tsfkostas@users.noreply.github.com>
2020-12-12 17:45:02 +0200

    createOrg, printDetails

--- Schools.java
+++ Schools.java
@@ -21,22 +21,36 @@ public class Schools extends Organisations implements caseManagmentAndHumanAddit
 
 	}
 
-	public static void printDetails(int code) {
-		//this method is used in displaySMenu
-		//prints all details of the specific school(FE name,region)
-		//with the code that takes,the code is similar the the check method ahead
-		//look caseManagmentAndHumanAddition
-	}
 	public static void callDeclareCase(int code) {
 		//for example allSchools.get(code).declareCase();
 	}
 	public void managementCases() {
 		//look caseManagmentAndHumanAddition
 	}
-	public static void createOrg() {
-		//look caseManagmentAndHumanAddition
-	}
+
 	public static void declareCase(int number) {
 		//look caseManagmentAndHumanAddition
+	}
+	public static void createOrg() {
+		while(true) {
+			System.out.println("What is the name of the school?/nWrite the name without spaces!");
+			String name = scanner.next();
+			System.out.println("In which area is your organization located?/nWrite the are without spaces!");
+			String area = scanner.next();
+			System.out.println("How many teachers does your organization have?");
+			scanner.nextLine();
+			int teachers = scanner.nextInt();
+			System.out.println("How many students does your organization have?");
+			int students = scanner.nextInt();
+			System.out.printf("Name School: %s, Area: %s, Total People(Teachers plus Students): %d.Is that correct? 1for yes",name,area,teachers+students);
+			if (scanner.nextLine().equals("1")) {
+				break;
+			}
+		}
+		Schools newOn = new Schools(name,area,teachers+students);
+	}
+
+	public static void printDetails(int code) {
+		System.out.println("Welcome.The" + allSchools.get(code).super.getName + "School of" + allSchools.get(code).super.getArea + "will take drastic measures to stop spread of covid-19 in our school.Please stay safe and we will call you soon.Always our first priority was the safety of our children!Thanks for understanding in these difficult times.");
 	}
 }
