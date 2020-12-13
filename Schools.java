056742344f46a6f9676847f978da3f4523df2e55
Λεωνιδας Διαμαντόπουλος <72790322+LeoDiam@users.noreply.github.com>
2020-12-13 09:51:05 +0200

    Update Schools.java
    solving some compiles errors...

--- Schools.java
+++ Schools.java
@@ -1,15 +1,18 @@
-package gr.projAboutCovid.leo.proj;
+package sample;
+
+import sample.Human;
+import sample.Organisations;
+import sample.caseManagmentAndHumanAddition;
 
 import java.util.Scanner;
 import java.util.ArrayList;
-public class Schools extends Organisations implements caseManagmentAndHumanAddition{
-	private Scanner scanner = new Scanner(System.in);
+public class Schools extends Organisations implements caseManagmentAndHumanAddition {
+	private static Scanner scanner = new Scanner(System.in);
 	private ArrayList<Schools> allSchools = new ArrayList<Schools>() ;
 	private static int count = 0; //use for our personal separation of the objects
 	private int id ;//personal id of each object
 	private ArrayList<Human> teachers = new ArrayList<Human>();
-	private ArrayList<Human> w
-	public Schools(String name, String area, int numbersOfPeople,) {
+	public Schools(String name, String area, int numbersOfPeople) {
 		//see if the arguments will be taken outside of the constructor or inside
 		//more info call christos
 		super(name, area, numbersOfPeople);
@@ -46,8 +49,9 @@ public class Schools extends Organisations implements caseManagmentAndHumanAddit
 			if (scanner.nextLine().equals("1")) {
 				break;
 			}
+			Schools newOn = new Schools(name,area,teachers+students);
 		}
-		Schools newOn = new Schools(name,area,teachers+students);
+
 	}
 
 	public static void printDetails(int code) {
