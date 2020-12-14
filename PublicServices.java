5cf052346c0f087b401705adf39b5b9aa43d0fc3
ChristosBaztekas <christosbaztekas@gmail.com>
2020-12-14 12:28:19 +0200

    Update PublicServices.java

--- PublicServices.java
+++ PublicServices.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 public class PublicServices extends Labors {
 	private final int id;
 	private static int count = 0;
-	private ArrayList<Human> employees;
 	static Scanner scanner = new Scanner(System.in);
 	private static ArrayList<PublicServices> allPublicServices = new ArrayList<PublicServices>();
 
 	public PublicServices(String name, String area, int numbersOfPeople) {
 		super(name, area, numbersOfPeople);
 		id = count ++;
+		allPublicServices.add(this);
 
 	}
 	@Override
