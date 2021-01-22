b37b0e56b97658ac7d76b6f748421b173bba311c
LeoDiam <t8190243@aueb.gr>
2021-01-22 14:24:00 +0200

    declare a case is active and totally functional for all the menus

--- CovidApp/src/covid/app/main/app/boolship/Main.java
+++ CovidApp/src/covid/app/main/app/boolship/Main.java
@@ -25,19 +25,19 @@ public class Main {
 //        } catch (IllegalAccessException e) {
 //            e.printStackTrace();
 //        }
-        launch(args);
+        //launch(args);
 
 
-//        GuiClass wsFrame = new GuiClass();
-//        wsFrame.setBounds(400, 100, 900, 700);
-//        wsFrame.setVisible(true);
-//        wsFrame.setTitle("Welcome to the app of case detection and contact detection!");
-//        wsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        a = args;// we need this for the piechart
+       GuiClass wsFrame = new GuiClass();
+        wsFrame.setBounds(400, 100, 900, 700);
+        wsFrame.setVisible(true);
+        wsFrame.setTitle("Welcome to the app of case detection and contact detection!");
+        wsFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        a = args;// we need this for the piechart
 
 
     }
-      @Override public void start(Stage stage) {
+      /*@Override public void start(Stage stage) {
             Scene scene = new Scene(new Group());
             stage.setTitle("Statistical Analysis");
             stage.setWidth(500);
@@ -55,5 +55,5 @@ public class Main {
             ((Group) scene.getRoot()).getChildren().add(chart);
             	stage.setScene(scene);
                 stage.show();
-        }
+        }*/
 }
